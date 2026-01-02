eab80689ba7234d0a7ba5aa73d007ebb53679cf0
Ctrl-Stack-Studio-Byte <curiousexplorer99ecfbp@outlook.com>
2026-01-02 23:44:03 +0800

    Add project files.

---
+++ Industrial.SignalSimulator1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentModbus;
+
+namespace Industrial.SignalSimulator1 {
+  internal class Program {
+    static void Main(string[] args) {
+      var server = new ModbusTcpServer();
+
+      short[] registers = new short[10];
+
+      Random random = new Random();
+
+      server.Start(new IPEndPoint(IPAddress.Any,5200));
+      Console.WriteLine("Modbus Server Started on Port 5200...");
+
+
+      while(true) {
+        var buffer = server.GetHoldingRegisters();
+        buffer[0] = (short) random.Next(200, 301) ;
+        buffer[1]++;
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] Temperature: {buffer[0]/10.0}°C | Count: {buffer[1]}.");
+        Thread.Sleep(1000);
+      }
+    }
+  }
+}
---
+++ Industrial.SignalSimulator1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Industrial.SignalSimulator1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Industrial.SignalSimulator1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a7b2471-b12c-4ebc-b4c1-634849ce5095")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
